0b8d0c0eec93454e180c05cdfd2c19dc8264d83e
Fuad Abdella <v-fabdella@microsoft.com>
2021-08-20 19:50:09 -0700

    added dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require("fs")
-const privateKey = fs.readFileSync(".secret").toString()
+const privateKey = fs.readFileSync(process.env.SECRET).toString()
 const projectId = "9f426a214d39475685e70473a3b568e0"
 
 
